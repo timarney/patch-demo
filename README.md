7686b7b7cb74db9094920efaf1002b9e86d7fd5c
Tim Arney <tim@line37.com>
2019-07-03 09:14:29 -0400

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # patch-demo
+
+Demo to help understand npm dependency resolution
+
+See: [Understanding npm dependency resolution](https://medium.com/learnwithrahul/understanding-npm-dependency-resolution-84a24180901b)
---
+++ index.js
@@ -0,0 +1,11 @@
+/* 
+Reference
+
+https://medium.com/learnwithrahul/understanding-npm-dependency-resolution-84a24180901b */
+
+const print1 = require("timarney-module1").print;
+const print2 = require("timarney-module2").print;
+const print3 = require("timarney-module3").print;
+print1();
+print2();
+print3();
